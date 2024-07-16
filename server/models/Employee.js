f76156a3ee7ce86ebf0e264dc7337c01d53f27b8
Md.Tousif <mdtousif8410@gmail.com>
2024-07-16 21:06:19 +0530

    Update Employee.js

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -3,22 +3,21 @@ const mongoose = require('mongoose');
 const EmployeeSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true, // Ensure the name is required
+        required: true, 
     },
     email: {
         type: String,
-        required: true, // Ensure the email is required
-        unique: true,    // Ensure the email is unique
-        lowercase: true, // Normalize email to lowercase
-        trim: true,      // Remove whitespace
+        required: true, 
+        unique: true,    
+        lowercase: true, 
+        trim: true,      
     },
     password: {
         type: String,
-        required: true, // Ensure the password is required
+        required: true, 
     },
 }, {
-    timestamps: true, // Automatically add createdAt and updatedAt fields
-});
+    timestamps: true, 
 
 const EmployeeModel = mongoose.model('Employee', EmployeeSchema);
 module.exports = EmployeeModel;
